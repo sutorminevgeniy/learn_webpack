847c26a3ccf6c3470fa5019ce91c4b88bca1ab75
sutorminevgen <sutorminevgen@gmail.com>
2021-06-01 02:25:13 +0300

    add watch

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   entry: './home', // точка входа
   output: {
     filename: 'build.js', // куда собирать
-    library: 'home' // для доступа к экспортированнным пременным этого модуля извне (home.welcome(...))
+    library: 'home', // для доступа к экспортированнным пременным этого модуля извне (home.welcome(...))
+  },
+
+  watch: true, // пересобирает сборку при обновление файлов
+  watchOptions: {
+    aggregateTimeout: 100, // время ожидания после изменения
   },
 };
