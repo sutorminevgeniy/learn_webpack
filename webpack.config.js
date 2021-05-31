76ae33488e3931ea34c502c0f777ef1f314b4fc8
sutorminevgen <sutorminevgen@gmail.com>
2021-06-01 01:58:48 +0300

    add library

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,6 @@ module.exports = {
   entry: './home', // точка входа
   output: {
     filename: 'build.js', // куда собирать
+    library: 'home' // для доступа к экспортированнным пременным этого модуля извне (home.welcome(...))
   },
 };
